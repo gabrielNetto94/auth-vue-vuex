307481ae814e4ae19c718b142233434ad09499d1
gabrielNetto94 <silveiranetto94@gmail.com>
2021-07-30 14:21:41 -0300

    Validar usário autenticado antes de entrar na rota

--- auth-vue-vuex/src/router/index.js
+++ auth-vue-vuex/src/router/index.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Gerentes from '../views/Gerentes.vue'
 import Home from '../views/Home.vue'
-import NovoUsuario from '../views/NovoUsuario.vue'
+//import NovoUsuario from '../views/NovoUsuario.vue'
 import Login from '../views/Login.vue';
+import provedor from '@/provedor';
 
 Vue.use(VueRouter)
 
@@ -21,17 +22,36 @@ const routes = [
   {
     path: '/cadastre-se',
     name: 'novo.usuario',
-    component: NovoUsuario
+    //lazy loading para carregar o arquivo apenas quando chamar esta rota
+    component : ( ) => import(/*webpackChunkName: "registrar" */'../views/NovoUsuario'),
+    meta: {
+      publica: true
+    }
   },
   {
     path: '/login',
     name: 'login',
-    component: Login
+    component: Login,
+    meta: {
+      //seta para true se página é publica ou privada
+      publica: true
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+//antes de cada rota testa se a rota é pública ou privada
+router.beforeEach((routeTo, routeFrom, next) => {
+  //se rota não for publica e não tiver token
+  if (!routeTo.meta.publica && !provedor.state.token) {
+
+    //manda pra login
+    return next({ path: '/login' })
+  }
+  next();
+});
+
+export default router;
--- auth-vue-vuex/src/views/Gerentes.vue
+++ auth-vue-vuex/src/views/Gerentes.vue
@@ -31,14 +31,14 @@ export default {
   },
   //chama este método antes do componente ser montado
   //para onde vamos "to"; de onde estamos indo "from";  e o que fazer "next"
-  beforeRouteEnter(to, from, next) {
-    //se não tem token, envia para rota login
-    if (!this.$store.state.token) {
-      next({ name: "login" });
-    }
-    //se tiver token vai para a rota que está sendo chamada, no caso a /gerentes
-    next();
-  },
+  // beforeRouteEnter(to, from, next) {
+  //   //se não tem token, envia para rota login
+  //   if (!this.$store.state.token) {
+  //     next({ name: "login" });
+  //   }
+  //   //se tiver token vai para a rota que está sendo chamada, no caso a /gerentes
+  //   next();
+  // },
 };
 </script>
 
