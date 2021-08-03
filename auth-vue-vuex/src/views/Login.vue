31590d3343faf20232c0107e640a3f5976ba201f
gabrielNetto94 <silveiranetto94@gmail.com>
2021-08-03 08:46:59 -0300

    Avisa na interface de erro ao logar

--- auth-vue-vuex/src/views/Login.vue
+++ auth-vue-vuex/src/views/Login.vue
@@ -10,6 +10,7 @@
         <label for="senha">Senha</label>
         <input type="password" class="form-control" v-model="usuario.senha" />
       </div>
+      <p class="alert alert-danger" v-if="mensagemErro">{{ mensagemErro }}</p>
       <button type="submit" class="btn btn-primary brn-block">Logar</button>
       <router-link :to="{ name: 'novo.usuario' }"
         >Não possui um cadastro, cadastre-se aqui!</router-link
@@ -23,13 +24,22 @@ export default {
   data() {
     return {
       usuario: {},
+      mensagemErro: "",
     };
   },
   methods: {
     efetuarLogin() {
       this.$store
         .dispatch("efetuarLogin", this.usuario)
-        .then(() => this.$router.push({ name: "gerentes" }));
+        .then(() => {
+          this.$router.push({ name: "gerentes" });
+          this.mensagemErro = "";
+        })
+        .catch((erro) => {
+          if (erro.request.status === 401) {
+            this.mensagemErro = "Login ou senha inválido(s)";
+          }
+        });
     },
   },
 };
